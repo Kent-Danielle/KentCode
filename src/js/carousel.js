6c1e0f72f97360aa7fcf54d1b14ae1a46214a308
Kent-Danielle <kconcengco@my.bcit.ca>
2022-09-14 22:50:09 -0700

    Added mobile view support for the main sections

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -4,6 +4,18 @@ const prevBtn = document.querySelector("#carousel_button-left");
 const nextBtn = document.querySelector("#carousel_button-right");
 const pagination = document.querySelector(".carousel__nav");
 const paginationBtns = Array.from(pagination.children);
+const projectPeekBtn = document.querySelector(".project-peek-btn");
+
+function resetProjectPeekBtn() {
+	const currentSlide = track.querySelector(".current-slide");
+	const currentText = currentSlide.querySelector(".carousel__slide-text");
+	const currentImg = currentSlide.querySelector(".carousel__slide-img");
+
+	currentText.classList.remove("hidden-project");
+	currentImg.classList.add("hidden-project");
+
+	projectPeekBtn.innerHTML = "Take a look!";
+}
 
 function sleep(ms) {
 	return new Promise((resolve) => setTimeout(resolve, ms));
@@ -43,6 +55,7 @@ const updatePagination = (currentBtn, targetBtnIndex) => {
 
 // left button
 prevBtn.addEventListener("click", (e) => {
+	resetProjectPeekBtn()
 	const currentSlide = track.querySelector(".current-slide");
 	const currentSlideIndex = slides.findIndex((slide) => slide === currentSlide);
 	const prevSlideIndex = (currentSlideIndex - 1).mod(slides.length);
@@ -57,6 +70,7 @@ prevBtn.addEventListener("click", (e) => {
 
 // right button
 nextBtn.addEventListener("click", (e) => {
+	resetProjectPeekBtn()
 	const currentSlide = track.querySelector(".current-slide");
 	const currentSlideIndex = slides.findIndex((slide) => slide === currentSlide);
 	const nextSlideIndex = (currentSlideIndex + 1) % slides.length;
@@ -71,6 +85,7 @@ nextBtn.addEventListener("click", (e) => {
 
 // pagination
 pagination.addEventListener("click", (e) => {
+	resetProjectPeekBtn()
 	const targetBtn = e.target.closest("button");
 	const currentBtn = pagination.querySelector(".current-slide");
 
@@ -82,4 +97,18 @@ pagination.addEventListener("click", (e) => {
 
 	moveToSlide(currentIndex < targetIndex, currentSlide, targetIndex);
 	updatePagination(currentBtn, targetIndex);
+});
+
+projectPeekBtn.addEventListener("click", (e) => {
+	const currentSlide = track.querySelector(".current-slide");
+	const currentText = currentSlide.querySelector(".carousel__slide-text");
+	const currentImg = currentSlide.querySelector(".carousel__slide-img");
+
+	currentText.classList.toggle("hidden-project");
+	currentImg.classList.toggle("hidden-project");
+
+	projectPeekBtn.innerHTML =
+		projectPeekBtn.innerHTML == "Take me back"
+			? "Take a look!"
+			: "Take me back";
 });
